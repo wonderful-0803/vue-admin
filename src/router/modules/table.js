9a33dd4422548ff04b96bfbb3307a965eb13476f
杨帆 <fan.yang@crgecent.com>
2021-06-13 16:25:31 +0800

    init

---
+++ src/router/modules/table.js
@@ -0,0 +1,23 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const tableRouter = {
+  path: '/table',
+  component: Layout,
+  redirect: '/table/complex-table',
+  name: 'Table',
+  meta: {
+    title: 'Table',
+    icon: 'table'
+  },
+  children: [
+    {
+      path: 'inline-edit-table',
+      component: () => import('@/views/table/inline-edit-table'),
+      name: 'InlineEditTable',
+      meta: { title: 'Inline Edit' }
+    }
+  ]
+}
+export default tableRouter
